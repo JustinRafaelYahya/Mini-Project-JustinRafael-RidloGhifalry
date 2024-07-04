30fad84c4f5c2e9d87345784780162847633afbb
JustinRafaelYahya <justin.rafael0@gmail.com>
2024-07-05 04:45:50 +0700

    feat: added event cards

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,13 @@
+"use client"
 import Hero from '@/components/Hero'
+import MainPage from '@/components/MainPage'
 import Image from 'next/image'
 
 export default function Home() {
   return (
 <div>
   <Hero />
+  <MainPage />
 </div>
   )
 }
