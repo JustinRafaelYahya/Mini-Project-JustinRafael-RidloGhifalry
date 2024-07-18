ce5fd1fe54e99b7ebf984c725c35ac7dd6810aa1
RidloGhifary <ridloghfry@gmail.com>
2024-07-18 11:19:22 +0700

    feat: create update event ui

---
+++ apps/web/src/app/events/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+import FormUpdate from './_components/FormUpdate';
+
+export default function EditEvent() {
+  return (
+    <div className="w-full max-w-7xl container mx-auto my-20 p-4">
+      <FormUpdate />
+    </div>
+  );
+}
