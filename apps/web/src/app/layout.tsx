aa1fdb436e7dd34bbb4cde15f4e1570b6504ded2
RidloGhifary <ridloghfry@gmail.com>
2024-07-20 13:19:53 +0700

    feat: complete navbar functionality

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import { Header } from '@/components/Header';
+import { Header } from '@/components/Navbar/Header';
 import { Footer } from '@/components/Footer';
 import { CurrentUserProvider } from '@/context/UserContext';
 
