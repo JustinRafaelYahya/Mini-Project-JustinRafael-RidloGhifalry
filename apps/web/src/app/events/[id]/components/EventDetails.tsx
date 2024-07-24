37daf81a40918066b387605ad3ec1bbfd4b1285a
RidloGhifary <ridloghfry@gmail.com>
2024-07-24 09:15:53 +0700

    feat: like event functionality

--- apps/web/src/app/events/[id]/components/EventDetails.tsx
+++ apps/web/src/app/events/[id]/components/EventDetails.tsx
@@ -2,17 +2,26 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 import { getEventById } from '@/api/route';
 import MainButton from '@/components/MainButton';
 import MainLink from '@/components/LinkMain';
-import React from 'react';
+import { FaHeart } from 'react-icons/fa';
+import { useCurrentUser } from '@/context/UserContext';
+import { likeEvent } from '@/api/events/route';
 
 const EventDetails = () => {
-  const { id } = useParams();
+  const { error: userError, loading: isUserLoading, user } = useCurrentUser();
+
   const [event, setEvent] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isUserLike, setIsUserLike] = useState<boolean>(false);
+  const [likeCount, setLikeCount] = useState<number>(event?.likes || 0);
+
+  const { id } = useParams();
+  const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     if (id) {
@@ -31,11 +40,48 @@ const EventDetails = () => {
     }
   }, [id]);
 
-  if (loading) {
+  useEffect(() => {
+    const isUserLikesEvent = () => {
+      if (user) {
+        const isUserLike = event?.liked?.find(
+          (like: any) =>
+            like?.user_id === user?.id && like?.event_id === event?.id,
+        );
+
+        setIsUserLike(isUserLike);
+      } else {
+        setIsUserLike(false);
+      }
+    };
+
+    isUserLikesEvent();
+  }, []);
+
+  const handleLikeEvent = async (id: number) => {
+    if (!user) {
+      router.push('/login');
+      return;
+    }
+
+    try {
+      const res = await likeEvent({ id: event?.id, path: pathname });
+      if (res?.message === 'Event unliked') {
+        setIsUserLike(false);
+        setLikeCount((prevCount) => prevCount - 1);
+      } else {
+        setLikeCount((prevCount) => prevCount + 1);
+        setIsUserLike(true);
+      }
+    } catch (err) {
+      alert('Sorry, something went wrong. Please try again later.');
+    }
+  };
+
+  if (loading || isUserLoading) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
+  if (error || userError) {
     return <div>{error}</div>;
   }
 
@@ -61,7 +107,7 @@ const EventDetails = () => {
           <p>{event.about}</p>
         </div>
         <p className="mt-4 mb-[-1rem] text-lg font-semibold">Time and Place:</p>
-        <ul className="p-5 flex flex-col list-disc mb-10">
+        <ul className="p-5 flex flex-col list-disc">
           <li className="list-disc my-1">{event.location}</li>
           <li className="list-disc my-1">
             From {event.start_event.substr(0, 10)} at {event.start_time}
@@ -70,9 +116,16 @@ const EventDetails = () => {
             To {event.end_event.substr(0, 10)} at {event.end_time}
           </li>
         </ul>
-        <p className="mt-2 font-semibold mb-2">
-          by {event.organizer.username}{' '}
-        </p>
+
+        <button
+          onClick={() => handleLikeEvent(event.id)}
+          className={`text-3xl p-1 hover:scale-105 w-fit transition duration-100 cursor-pointer flex items-center gap-2`}
+        >
+          <FaHeart color={isUserLike ? 'red' : 'black'} />
+          <span className="text-sm text-black">{likeCount}</span>
+        </button>
+
+        <p className="mt-2 font-semibold mb-2">by {event.organizer.username}</p>
         <MainButton className="lg:mt-10 w-full">Purchase</MainButton>
       </div>
     </section>
