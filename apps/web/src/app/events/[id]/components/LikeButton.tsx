59e64436b358c9b24f50ed4aa925e660df9e6117
RidloGhifary <ridloghfry@gmail.com>
2024-07-28 11:41:29 +0700

    perf: like button improve code quality

--- apps/web/src/app/events/[id]/components/LikeButton.tsx
+++ apps/web/src/app/events/[id]/components/LikeButton.tsx
@@ -2,7 +2,7 @@ import { likeEvent } from '@/api/events/route';
 import { UserProps } from '@/interfaces/user';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { FaHeart } from 'react-icons/fa';
 
 export default function LikeButton({
@@ -12,28 +12,15 @@ export default function LikeButton({
   event: any;
   user: UserProps;
 }) {
-  const [isUserLike, setIsUserLike] = useState<boolean>(false);
+  const [isUserLike, setIsUserLike] = useState<boolean>(
+    event?.liked?.some(
+      (like: any) => like?.user_id === user?.id && like?.event_id === event?.id,
+    ) || false,
+  );
   const [likeCount, setLikeCount] = useState<number>(event?.likes || 0);
 
   const router = useRouter();
   const pathname = usePathname();
-
-  useEffect(() => {
-    const isUserLikesEvent = () => {
-      if (user) {
-        const isUserLike = event?.liked?.find(
-          (like: any) =>
-            like?.user_id === user?.id && like?.event_id === event?.id,
-        );
-
-        setIsUserLike(isUserLike);
-      } else {
-        setIsUserLike(false);
-      }
-    };
-
-    isUserLikesEvent();
-  }, [event, user]);
 
   const handleLikeEvent = async (id: number) => {
     if (!user) {
@@ -43,13 +30,11 @@ export default function LikeButton({
 
     try {
       const res = await likeEvent({ id, path: pathname || '' });
-      if (res?.message === 'Event unliked') {
-        setIsUserLike(false);
-        setLikeCount((prevCount) => prevCount - 1);
-      } else {
-        setLikeCount((prevCount) => prevCount + 1);
-        setIsUserLike(true);
-      }
+      const newLikeCount =
+        res?.message === 'Event unliked' ? likeCount - 1 : likeCount + 1;
+      const newIsUserLike = res?.message === 'Event unliked' ? false : true;
+      setLikeCount(newLikeCount);
+      setIsUserLike(newIsUserLike);
     } catch (err) {
       alert('Sorry, something went wrong. Please try again later.');
     }
@@ -58,7 +43,7 @@ export default function LikeButton({
   return (
     <button
       onClick={() => handleLikeEvent(event.id)}
-      className={`text-3xl p-1 hover:scale-105 w-fit transition duration-100 cursor-pointer flex items-center gap-2`}
+      className="text-3xl p-1 hover:scale-105 w-fit transition duration-100 cursor-pointer flex items-center gap-2"
     >
       <FaHeart color={isUserLike ? 'red' : 'black'} />
       <span className="text-sm text-black">{likeCount}</span>
